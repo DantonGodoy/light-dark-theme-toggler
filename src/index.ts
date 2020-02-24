3e5f4abab10a9bdfd0a4ac6d6a100e949e7c5558
Danton de Godoy Petri Lima <dantongplima@gmail.com>
2020-02-24 19:01:03 -0300

    chore(): continues

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
-// import Ryu from './images/ryu-wallpaper';
 import './styles';
 
-const toggleBtn = document.querySelector('.theme-toggle-button'),
-      sunMoonContainer = document.querySelector('.sun-moon-container');
+const toggleBtn = document.querySelector('.theme-toggle-button')! as HTMLElement,
+      sunMoonContainer = document.querySelector('.sun-moon-container')! as HTMLElement,
+      currentYear = document.querySelector('#currentYear')! as HTMLElement;
 
+/**
+ * Applies toggle theme on button click by applying the .dark class to the body element.
+ * Gets property --rotation value from the sun and moon container computed style and applies a positive 180deg value so the sun and moon always spins on a positive direction.
+ */
 toggleBtn.addEventListener('click', () => {
   document.body.classList.toggle('dark');
   const currentRotation = parseInt(getComputedStyle(sunMoonContainer).getPropertyValue('--rotation'))
   sunMoonContainer.style.setProperty('--rotation', currentRotation + 180)
 });
+
+/**
+ * Updates footer current year.
+ */
+const updateYear = () => {
+  const date: Date = new Date();
+  currentYear.textContent = date.getFullYear();
+};
+
+updateYear();
